999762b27ba5464efe6ea005718e0e63fa5e855c
Steven Retz <sretz@innovoft.com>
2023-01-12 13:06:26 -0300

    added Benchmarks

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			BenchmarkRunner.Run(typeof(Program).Assembly);
+		}
+	}
+}
---
+++ Benchmarks/StringCreationBenchmarks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Innovoft.Text;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+	[MemoryDiagnoser]
+	public class StringCreationBenchmarks
+	{
+		#region Constants
+		private const string text = "Decoding";
+		#endregion //Constants
+
+		#region Class Fields
+		private static readonly byte[] encoded = Encoding.UTF8.GetBytes(text);
+		private static readonly StringDecodingTree tree = new StringDecodingTree();
+		#endregion //Class Fields
+
+		#region Class Constructor
+		static StringCreationBenchmarks()
+		{
+			tree.Add(encoded, 0, encoded.Length, text);
+		}
+		#endregion //Class Constructor
+
+		[Benchmark]
+		public string EncodingGeString()
+		{
+			return Encoding.UTF8.GetString(encoded, 0, encoded.Length);
+		}
+
+		[Benchmark]
+		public string StringDecodingTreeGet()
+		{
+			return tree.GetValue(encoded, 0, encoded.Length);
+		}
+	}
+}
